e89dd789ab3e0ad70da0c47bf80f838b4e2d5db3
bramadevu45 <bramadevu@albany.edu>
2017-12-05 21:34:34 -0800

    Adding Api key and some Request urls

---
+++ app/src/main/java/com/project/lunchroomfinder/lunchroomfinderapp/Commons/CommonMains/RequestConstants.java
@@ -0,0 +1,14 @@
+package com.project.lunchroomfinder.lunchroomfinderapp.Commons.CommonMains;
+
+/**
+ * Created by Ramadevu Srinivas on 12/5/2017.
+ */
+
+public class RequestConstants {
+
+    public static final String API_KEY = "3ae50044a116bd02be14ec45a4debbfe";
+    public static final String USER_KEY_NAME = "user-key";
+    public static final String CATEGORY_REQUEST = "https://developers.zomato.com/api/v2.1/categories";
+    public static final String CITY_REQUEST_ID = "https://developers.zomato.com/api/v2.1/cities";
+
+}
